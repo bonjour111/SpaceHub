b0cb7e1d0a4671e1fc6b3e61893ea54479a25582
Leonardo Pirro <leonardo.pirro@gmail.com>
2023-01-17 14:27:14 +0100

    Add Licence and Copyright (#2)
    * Add Licence and Copyright to the project
    
    #861m85v2b
    
    * Fix Ktlint
    
    Co-authored-by: lpirro <leonardo.pirro@wombatinvest.com>

--- data/repository/src/main/java/com/lpirro/repository/di/RepositoryModule.kt
+++ data/repository/src/main/java/com/lpirro/repository/di/RepositoryModule.kt
@@ -1,6 +1,25 @@
+/*
+ *
+ * SpaceHub - Designed and Developed by LPirro (Leonardo Pirro)
+ * Copyright (C) 2023 Leonardo Pirro
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package com.lpirro.repository.di
 
-import android.content.Context
 import com.lpirro.domain.repository.LaunchDetailRepository
 import com.lpirro.domain.repository.LaunchesRepository
 import com.lpirro.network.SpaceHubApiService
@@ -34,7 +53,6 @@ import com.lpirro.repository.mapper.YouTubeVideoIdParserImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 
 @Module
